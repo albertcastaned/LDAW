3b28b03418b725165e01287d140b6c1d2f576601
Alberto Castañeda <albcastar@gmail.com>
2020-04-15 17:10:38 -0700

    Agregar fixtures para pruebas

---
+++ Sistema/aplicacion/compras_ventas/routes.py
@@ -0,0 +1,4 @@
+from flask import render_template, Blueprint
+
+compras_ventas = Blueprint('compras_ventas', __name__, template_folder='templates')
+ 
---
+++ Sistema/tests/test_productos.py
@@ -0,0 +1,5 @@
+from punto_venta.productos.modelo import *
+from .conftest import app, client
+
+def test_placeholder():
+    pass
