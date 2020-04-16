452cb24fa81e5e6a24f6f8e4e57448aa99be52c9
Alberto Castañeda <albcastar@gmail.com>
2020-04-16 15:44:15 -0700

    Agregar primer version de login logout

--- Sistema/punto_venta/usuarios/routes.py
+++ Sistema/punto_venta/usuarios/routes.py
@@ -1,7 +1,8 @@
 from flask import render_template, url_for, flash, redirect, request, Blueprint
-from punto_venta.usuarios.forms import RegistarUsuarioForm
+from punto_venta.usuarios.forms import *
 from punto_venta.models import Usuario
 from punto_venta import db, bcrypt
+from flask_login import login_user, current_user, logout_user, login_required
 
 usuarios = Blueprint('usuarios', __name__, template_folder='templates')
 
@@ -23,3 +24,23 @@ def usuarios_lista():
     page = request.args.get('pagina', 1, type=int)
     usuarios = Usuario.query.paginate(page=page, per_page=10)
     return render_template('usuarios_lista.html', usuarios=usuarios, titulo="Lista de Usuarios")
+
+@usuarios.route("/usuarios/login", methods=['GET','POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = Usuario.query.filter_by(nombre_usuario=form.nombre_usuario.data).first()
+        if user and bcrypt.check_password_hash(user.contrasenia, form.contrasenia.data):
+            login_user(user)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('main.index'))
+        else:
+            flash('Inicio de Sesion no fue exitoso. Verifica los campos', 'danger')
+    return render_template('login.html', titulo='Iniciar Sesion', form=form)
+
+@usuarios.route("/usuarios/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('usuarios.login'))
