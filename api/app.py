6e1075400cb9a92c19530bcb9271d2ca9c651ff2
Alberto Castañeda <albcastar@gmail.com>
2020-05-22 14:23:52 -0700

    Agregar compatibilidad con MySQL

--- api/app.py
+++ api/app.py
@@ -4,9 +4,13 @@ from flask_restful import Api
 
 from database.db import initialize_db
 from resources.routes import initialize_routes
-
+print(os.environ['HOME'])
 class Config():
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
+    if(os.environ.get('MYSQL') is None):
+        SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
+    else:
+        SQLALCHEMY_DATABASE_URI = os.environ['MYSQL']
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = True
     SECRET_KEY = os.urandom(24)
@@ -15,7 +19,7 @@ app = Flask(__name__)
 app.config.from_object(Config)
 
 api = Api(app, prefix="/api/v1")
-
+print("Database: " + app.config['SQLALCHEMY_DATABASE_URI'])
 
 initialize_db(app)
 initialize_routes(api)
